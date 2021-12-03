8913f805a63fe561da68fcd98b88b520d3e4c8d7
Matías Quezada <matias.traducciones@gmail.com>
2021-12-03 02:05:15 -0300

    vista home con carousel

--- src/views/CollabForm.jsx
+++ src/views/CollabForm.jsx
@@ -5,50 +5,82 @@ function CollabForm() {
   const { store, actions } = useContext(Context);
   return (
     <>
-      <div classNameName="container">
-        <div classNameName="row">
-          <p>
-            Eres mas que un aficionado de los honguitos y tienes muchas
-            observaciones de hongos. Acá es el lugar donde puedes compartir con
-            entusiastas y expertos del reino fungi todos tus hallazgos.
-          </p>
-          <p>
-            También puedes interactuar con usuarios regulares contestando sus
-            dudas o respondiendo sus formularios de reconocimiento.
-          </p>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="contenido mt-3 mb-5">
+              <p>
+                Eres mas que un aficionado de los honguitos y tienes muchas
+                observaciones de hongos. Acá es el lugar donde puedes compartir
+                con entusiastas y expertos del reino fungi todos tus hallazgos.
+              </p>
+              <p>
+                También puedes interactuar con usuarios regulares contestando
+                sus dudas o respondiendo sus formularios de reconocimiento.
+              </p>
+            </div>
+          </div>
         </div>
         <div className="row">
           <div className="col-8">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Ingresa la especie"
-              aria-label="Ingresa la especie"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Ingresa la localidad"
-              aria-label="Ingresa la localidad"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Sobre que sustrato estaba creciendo el honguito (Tierra, madera, piedra)"
-              aria-label="Sobre que sustrato estaba creciendo el honguito (Tierra, madera, piedra)"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Laminillas"
-              aria-label="Laminillas"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Poros"
-              aria-label="Poros"
-            />
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="especie"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Ingresa la especie"
+                aria-label="Ingresa la especie"
+              />
+            </div>
+            <div className="col 8">
+              <div className="contenido mb-3">
+                <input
+                  type="text"
+                  name="localidad"
+                  onChange={(e) => actions.handleChange(e)}
+                  className="pt-2 form-control"
+                  placeholder="Ingresa la localidad"
+                  aria-label="Ingresa la localidad"
+                />
+              </div>
+            </div>
+            <div className="col 8">
+              <div className="contenido mb-3">
+                <input
+                  type="text"
+                  name="sustrato"
+                  onChange={(e) => actions.handleChange(e)}
+                  className="form-control pt-2"
+                  placeholder="Sobre que sustrato estaba creciendo el honguito (Tierra, madera, piedra)"
+                  aria-label="Sobre que sustrato estaba creciendo el honguito (Tierra, madera, piedra)"
+                />
+              </div>
+            </div>
+            <div className="col 8">
+              <div className="contenido mb-3">
+                <input
+                  type="text"
+                  name="laminillas"
+                  onChange={(e) => actions.handleChange(e)}
+                  className="form-control pt-2"
+                  placeholder="Laminillas"
+                  aria-label="Laminillas"
+                />
+              </div>
+            </div>
+            <div className="col 8">
+              <div className="contenido mb-3">
+                <input
+                  type="text"
+                  name="poros"
+                  onChange={(e) => actions.handleChange(e)}
+                  className="form-control"
+                  placeholder="Poros"
+                  aria-label="Poros"
+                />
+              </div>
+            </div>
           </div>
         </div>
         <div className="row">
@@ -56,94 +88,143 @@ function CollabForm() {
             <label for="inputEmail4" className="form-label">
               Pileo
             </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Diametro"
-              aria-label="Diametro"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Forma"
-              aria-label="Forma"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Color"
-              aria-label="Color"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Margen"
-              aria-label="Margen"
-            />
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="diametro"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Diametro"
+                aria-label="Diametro"
+              />
+            </div>
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="forma"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Forma"
+                aria-label="Forma"
+              />
+            </div>
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="color"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Color"
+                aria-label="Color"
+              />
+            </div>
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="margen"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Margen"
+                aria-label="Margen"
+              />
+            </div>
           </div>
           <div className="col">
             <label for="inputEmail4" className="form-label">
               Pie
             </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Altura"
-              aria-label="Altura"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Color"
-              aria-label="Color"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Anillo"
-              aria-label="Anillo"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Diametro"
-              aria-label="Diametro"
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Volva"
-              aria-label="Volva"
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Adjunta las fotos de tu hongo observado"
-              aria-label="Adjunta las fotos de tu hongo observado"
-            />
-            <a className="btn btn-success">Subir imagen</a>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Adjunta las fotos de su esporada (solo si las tienes)"
-              aria-label="Adjunta las fotos de su esporada (solo si las tienes)"
-            />
-            <a className="btn btn-success">Subir imagen</a>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <div class="input-group">
-            <span class="input-group-text">Descripción</span>
-            <textarea class="form-control" aria-label="With textarea"></textarea>
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="altura"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Altura"
+                aria-label="Altura"
+              />
             </div>
-            <button type="submit" className="btn btn-success">
-                  Enviar formulario
-                </button>
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="color"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Color"
+                aria-label="Color"
+              />
+            </div>
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="anillo"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Anillo"
+                aria-label="Anillo"
+              />
+            </div>
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="diametro"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Diametro"
+                aria-label="Diametro"
+              />
+            </div>
+            <div className="contenido mb-5">
+              <input
+                type="text"
+                name="volva"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Volva"
+                aria-label="Volva"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="contenido mb-3">
+              <input
+                type="text"
+                name="add-img"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Adjunta las fotos de tu hongo observado"
+                aria-label="Adjunta las fotos de tu hongo observado"
+              />
+              <a className="btn btn-success">Subir imagen</a>
+            </div>
+            <div className="contenido mb-3">
+              <input
+                type="file"
+                name="add-esporada"
+                onChange={(e) => actions.handleChange(e)}
+                className="form-control"
+                placeholder="Adjunta las fotos de su esporada (solo si las tienes)"
+                aria-label="Adjunta las fotos de su esporada (solo si las tienes)"
+              />
+              <a className="btn btn-success">Subir imagen</a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="contenido mb-5">
+              <div class="input-group">
+                <span class="input-group-text">Descripción</span>
+                <textarea
+                  class="form-control"
+                  aria-label="With textarea"
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-success">
+                Enviar formulario
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,63 +4,202 @@ function Home() {
   return (
     <>
       <div className="container-fluid">
-        <div class="container-fluid bg-dark text-light p-5">
-          <a href="#" class="btn btn-primary">
+        <div className="container-fluid bg-dark text-light">
+          <a href="#" className="btn btn-primary float-end mt-3">
             Iniciar Sesión
           </a>
-          <div class="container bg-dark p-5">
-            <h1 class="display-4">Jumbotron de la myco app</h1>
+          <div className="container bg-dark p-3">
+            <h1 className="display-4">Jumbotron de la myco app</h1>
             <hr />
             <p>Myco App grupo Bastián Moral y Matías Quezada</p>
           </div>
         </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-4">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    En esta sección explicaremos que es un hongo cuales son su
-                    partes y que es todo este reino fungi para los neófitos del
-                    tema que no sepan nada sobre el asunto.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
+
+          <div className="container">
+            <div className="row mt-5">
+              <div className="col">
+                <div className="carousel slide carousel-fade" id="mi-carousel" data-bs-ride="carousel">
+                  <div className="carousel-inner">
+                    <div className="carousel-item active">
+                      <img className="img-fluid" src="../images/cordyceps-millitaris.jpg" alt="" />
+                    </div>
+                    <div className="carousel-item" data-bs-interval="5000">
+                      <div className="carousel-caption">
+                        <h5>Reino fúngico</h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                      </div>
+                      <img className="img-fluid" src="../images/hongo2.jpg" alt="" />
+                    </div>
+                    <div className="carousel-item">
+                      <img className="img-fluid" src="../images/hongo3.jpg" alt="" />
+                    </div>
+                  </div>
+                  <button className="carousel-control-prev" type="button" data-bs-target="#mi-carousel" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Anterior</span>
+                  </button>
+                  <button className="carousel-control-next" type="button" data-bs-target="#mi-carousel" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Siguiente</span>
+                  </button>
+                  <div className="carousel-indicators">
+                    <button type="button" className="active" data-bs-target="#mi-carousel" data-bs-slide-to="0" aria-label="Slide 1"></button>
+                    <button type="button" className="" data-bs-target="#mi-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" className="" data-bs-target="#mi-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-4"></div>
-            <div className="col-4">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    En esta sección dirigimos a la app donde el usuario podrá
-                    encontrar una gran variedad de hongos entre ellos muchos
-                    hongos comestibles o tóxicos con muchas características
-                    bastante particulares.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-              <div class="card row text-center">
-                <div class="card-header col-12">Logo 4Geeks</div>
-                <div class="card-body col-12">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text">
-                    FOOTER CON TODOS NUESTROS DATOS Y APARTADO DE CONTACTOS
-                  </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-                <div class="card-footer col-12 text-muted">2 days ago</div>
+          </div>
+
+        <div className="row justify-content-between">
+          <div className="col-4 mt-5 mb-5">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">¿Qué es un hongo?</h5>
+                <p className="card-text">
+                  En esta sección explicaremos que es un hongo cuales son su
+                  partes y que es todo este reino fungi para los neófitos del
+                  tema que no sepan nada sobre el asunto.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Saber más
+                </a>
               </div>
             </div>
+          </div>
+          <div className="col-4 mt-5 mb-5">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Myco App</h5>
+                <p className="card-text">
+                  En esta sección dirigimos a la app donde el usuario podrá
+                  encontrar una gran variedad de hongos entre ellos muchos
+                  hongos comestibles o tóxicos con muchas características
+                  bastante particulares.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Ir a la App
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <footer className="text-center text-lg-start bg-light text-muted">
+              <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+                <div className="me-5 d-none d-lg-block">
+                  <span>Get connected with us on social networks:</span>
+                </div>
+                <div>
+                  <a href="" className="me-4 text-reset">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="" className="me-4 text-reset">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a href="" className="me-4 text-reset">
+                    <i className="fab fa-google"></i>
+                  </a>
+                  <a href="" className="me-4 text-reset">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                  <a href="" className="me-4 text-reset">
+                    <i className="fab fa-linkedin"></i>
+                  </a>
+                  <a href="" className="me-4 text-reset">
+                    <i className="fab fa-github"></i>
+                  </a>
+                </div>
+              </section>
+              <section className="">
+                <div className="container text-center text-md-start mt-5">
+                  <div className="row mt-3">
+                    <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                      <h6 className="text-uppercase fw-bold mb-4">
+                        <i className="fas fa-gem me-3"></i>Myco App
+                      </h6>
+                      <p>
+                        Here you can use rows and columns to organize your
+                        footer content. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit.
+                      </p>
+                    </div>
+                    <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                      <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          Angular
+                        </a>
+                      </p>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          React
+                        </a>
+                      </p>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          Vue
+                        </a>
+                      </p>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          Laravel
+                        </a>
+                      </p>
+                    </div>
+                    <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                      <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          Pricing
+                        </a>
+                      </p>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          Settings
+                        </a>
+                      </p>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          Orders
+                        </a>
+                      </p>
+                      <p>
+                        <a href="#!" className="text-reset">
+                          Help
+                        </a>
+                      </p>
+                    </div>
+                    <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                      <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                      <p>
+                        <i className="fas fa-home me-3"></i> New York, NY 10012, US
+                      </p>
+                      <p>
+                        <i className="fas fa-envelope me-3"></i>
+                        info@example.com
+                      </p>
+                      <p>
+                        <i className="fas fa-phone me-3"></i> + 01 234 567 88
+                      </p>
+                      <p>
+                        <i className="fas fa-print me-3"></i> + 01 234 567 89
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <div
+                className="text-center p-4"
+              >
+                © 2021 Copyright:
+                <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
+                  MDBootstrap.com
+                </a>
+              </div>
+            </footer>
           </div>
         </div>
       </div>
